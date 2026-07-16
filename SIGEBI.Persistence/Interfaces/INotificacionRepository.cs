b7422bd15b47a1dce66ae0481d4170a10f6ce7e2
Welinton Corporan <welintoncorporanfabal@gmail.com>
2026-07-15 22:47:30 -0400

    feat(persistence): agregue métodos específicos por entidad en repositorios

--- SIGEBI.Persistence/Interfaces/INotificacionRepository.cs
+++ SIGEBI.Persistence/Interfaces/INotificacionRepository.cs
@@ -1,10 +1,11 @@
-﻿using SIGEBI.Domain.Entities.Notifications;
+﻿using SIGEBI.Domain.Base;
+using SIGEBI.Domain.Entities.Notifications;
 using SIGEBI.Domain.Repository;
 
 namespace SIGEBI.Persistence.Interfaces
 {
     public interface INotificacionRepository : IBaseRepository<Notificacion>
     {
-
+        Task<OperationResult> GetNotificacionesByUsuarioId(int usuarioId);
     }
 }
--- SIGEBI.Persistence/Repositories/Notifications/NotificacionRepository.cs
+++ SIGEBI.Persistence/Repositories/Notifications/NotificacionRepository.cs
@@ -1,4 +1,6 @@
-﻿using SIGEBI.Domain.Entities.Notifications;
+﻿using Microsoft.EntityFrameworkCore;
+using SIGEBI.Domain.Base;
+using SIGEBI.Domain.Entities.Notifications;
 using SIGEBI.Persistence.Base;
 using SIGEBI.Persistence.Context;
 using SIGEBI.Persistence.Interfaces;
@@ -7,8 +9,30 @@ namespace SIGEBI.Persistence.Repositories.Notifications
 {
     public class NotificacionRepository : BaseRepository<Notificacion>, INotificacionRepository
     {
+        private readonly LibraryContext _context;
+
         public NotificacionRepository(LibraryContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task<OperationResult> GetNotificacionesByUsuarioId(int usuarioId)
+        {
+            OperationResult result = new OperationResult();
+            try
+            {
+                var datos = await _context.Notificaciones
+                    .Where(n => n.UsuarioId == usuarioId)
+                    .OrderByDescending(n => n.EnviadoEn)
+                    .ToListAsync();
+                result.Data = datos;
+            }
+            catch (Exception)
+            {
+                result.Success = false;
+                result.Message = "Ocurrio un error obteniendo las notificaciones del usuario.";
+            }
+            return result;
         }
     }
 }
